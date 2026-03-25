fdb6480330a0a0f2ee0d7a488e01a20f0de1882b
ZZ-626 <63528721+ZZ-626@users.noreply.github.com>
2026-03-25 19:15:13 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -20,25 +20,32 @@ class Dataset(torch.utils.data.Dataset):
         Note:
             Make sure to put the files as the following structure:
             <dataset name>
-            ├── images
-            |   ├── 0a7e06.jpg
-            │   ├── 0aab0a.jpg
-            │   ├── 0b1761.jpg
-            │   ├── ...
-            |
-            └── masks
-                ├── 0
-                |   ├── 0a7e06.png
-                |   ├── 0aab0a.png
-                |   ├── 0b1761.png
-                |   ├── ...
-                |
-                ├── 1
-                |   ├── 0a7e06.png
-                |   ├── 0aab0a.png
-                |   ├── 0b1761.png
-                |   ├── ...
-                ...
+            ├── train/
+            │   ├── images
+            │   ├── 0a7e06.jpg
+            │   │   ├── 0aab0a.jpg
+            │   │   ├── 0b1761.jpg
+            │   │   ├── ...
+            │   |
+            │   └── masks
+            │       ├── 0
+            │       |   ├── 0a7e06.png
+            │       |   ├── 0aab0a.png
+            │       |   ├── 0b1761.png
+            │       |   ├── ...
+            │       |
+            │       ├── 1
+            │       |   ├── 0a7e06.png
+            │       |   ├── 0aab0a.png
+            │       |   ├── 0b1761.png
+            │       |   ├── ...
+            ├── val/
+            │   ├── images/
+            │   └── masks/
+            └── test/
+            ├── images/
+            └── masks/
+
         """
         self.img_ids = img_ids
         self.img_dir = img_dir
